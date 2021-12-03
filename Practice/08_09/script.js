7d60f341e68f2a54b8066addb39accf481eb29f8
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-12-03 13:03:03 +0200

    passed values between functions

--- Practice/08_09/script.js
+++ Practice/08_09/script.js
@@ -24,6 +24,8 @@ const frogpack = {
   },
   lidOpen: false,
   image: "../../assets/images/frog.svg",
+  description:
+    "A green kids backpack designed to make the lid look like the face of a frog",
   toggleLid: function (lidStatus) {
     this.lidOpen = lidStatus;
   },
@@ -34,26 +36,52 @@ const frogpack = {
 };
 
 // Baseline HTML output
+
 const content = `
-    <h1 class="backpack__name">${frogpack.name}</h1>
-    <ul class="backpack__features">
-      <li class="packprop backpack__volume">Volume:<span> ${
-        frogpack.volume
-      }l</span></li>
-      <li class="packprop backpack__color">Color:<span> ${
-        frogpack.color
-      }</span></li>
-      <li class="packprop backpack__pockets">Number of pockets:<span> ${
-        frogpack.pocketNum
-      }</span></li>
-      <li class="packprop backpack__strap">Left strap length:<span> ${
-        frogpack.strapLength.left
-      } inches</span></li>
-      <li class="packprop backpack__strap">Right strap length:<span> ${
-        frogpack.strapLength.right
-      } inches</span></li>
-      <li class="feature backpack__lid">Lid status:<span> ${
-        frogpack.lidOpen ? "open" : "closed"
-      }</span></li>
-    </ul>  
+<h1 class="backpack__name">${frogpack.name}</h1>
+<ul class="backpack__features">
+<li class="packprop backpack__volume">Volume:<span> ${
+  frogpack.volume
+}l</span></li>
+<li class="packprop backpack__color">Color:<span> ${frogpack.color}</span></li>
+<li class="packprop backpack__pockets">Number of pockets:<span> ${
+  frogpack.pocketNum
+}</span></li>
+<li class="packprop backpack__strap">Left strap length:<span> ${
+  frogpack.strapLength.left
+} inches</span></li>
+<li class="packprop backpack__strap">Right strap length:<span> ${
+  frogpack.strapLength.right
+} inches</span></li>
+<li class="feature backpack__lid">Lid status:<span> ${
+  frogpack.lidOpen ? "open" : "closed"
+}</span></li>
+</ul>
 `;
+
+const addFigure = (object) => {
+  let newFig = document.createElement("figure");
+  let newImg = document.createElement("img");
+  newImg.setAttribute("src", object.image);
+  newImg.setAttribute("alt", object.name);
+  let newCaption = document.createElement("figcaption");
+  newCaption.innerText = object.description;
+  newFig.append(newImg, newCaption);
+  return newFig;
+};
+
+const newBag = (object) => {
+  const newArticle = document.createElement("article");
+  newArticle.innerHTML = content;
+  newArticle.prepend(addFigure(object));
+  return newArticle;
+};
+// const newArticle = function (newName, newVolume, newColor) {
+//   document.createElement("article");
+//   return (newArticle.innerHTML = newBag(newName, newVolume, newColor));
+// };
+
+document.querySelector("main").appendChild(newBag(frogpack));
+document.querySelector("figure").classList.add("backpack__image");
+document.querySelector("main").classList.add("maincontent");
+document.querySelector("article").classList.add("backpack");
