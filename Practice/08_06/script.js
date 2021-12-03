c1e68d583f5983fcd7410f5f9071426eb5d5e9cc
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-12-03 12:03:03 +0200

    append article

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,6 +5,30 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+//Appenf new article to the DOM
+const greenPack = {
+  name: "Frog Pack",
+  color: "green",
+  volume: 8,
+  pocketNum: 3,
+};
+
+const addPack = function (currentPack) {
+  const newArticle = document.createElement("article");
+  newArticle.innerHTML = `
+    <h1>${currentPack.name}</h1>
+    <ul>
+      <li>Volume: ${currentPack.volume}</li>
+      <li>Color: ${currentPack.color}</li>
+      <li>Number of pockets: ${currentPack.pocketNum}</li>
+    </ul>
+  `;
+  return newArticle;
+};
+
+const main = document.querySelector("main");
+main.append(addPack(greenPack));
+
 // Function declaration
 function changeColor(tag, color) {
   const element = document.querySelector(`${tag}`);
@@ -21,7 +45,7 @@ const newColor = function (tag, color) {
 
 newColor("main", "yellow");
 
-// Arrow function
+// // Arrow function
 const lastColor = (tag, color) => {
   // const attribute = document.querySelector("iframe").getAttribute(`${attr}`);
   return (document.querySelector(`${tag}`).style.backgroundColor = `${color}`);
