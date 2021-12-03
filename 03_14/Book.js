609f9aa3831a207e2af4ef06436939a7113e2f66
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-12-03 14:11:11 +0200

    created new Book object with class

---
+++ 03_14/Book.js
@@ -0,0 +1,23 @@
+class Book {
+  constructor(name, author, category, pages, status, datePublished) {
+    this.name = name;
+    this.author = author;
+    this.category = category;
+    this.pages = pages;
+    this.status = status;
+    this.datePublished = datePublished;
+  }
+  changeStatus(newStatus) {
+    this.status = newStatus;
+  }
+
+  bookAge() {
+    let now = new Date();
+    let published = new Date(this.datePublished);
+    let elapsed = now - published; // elapsed time in milliseconds
+    let yearsSincePublished = Math.floor(elapsed / (1000 * 3600 * 24 * 356));
+    return yearsSincePublished;
+  }
+}
+
+export default Book;
--- 03_14/script.js
+++ 03_14/script.js
@@ -1,11 +1,12 @@
 /**
  * Challenge: Create a new object type
- * 
+ *
  * - Create a new object type "Book" using a class or an object constructor function.
  * - Add at least 5 book objects.
  */
 
 import Backpack from "./Backpack.js";
+import Book from "./Book.js";
 
 const everydayPack = new Backpack(
   "Everyday Backpack",
@@ -21,3 +22,30 @@ const everydayPack = new Backpack(
 console.log("The everydayPack object:", everydayPack);
 console.log("The pocketNum value:", everydayPack.pocketNum);
 console.log("Days since aquired:", everydayPack.backpackAge());
+
+const eloquent = new Book(
+  "Eloquent JavaScript",
+  "Marijn Haverbeke",
+  "Coding",
+  449,
+  "new",
+  "January 4, 2017 10:00:00 PST"
+);
+
+console.log(`I'm reading now "${eloquent.name}", by ${eloquent.author}`);
+console.log("This book is", eloquent.bookAge(), "years old.");
+
+const whatITalk = new Book(
+  "What I Talk About When I Talk About Running",
+  "Haruki Murakami",
+  "Running",
+  188,
+  "used",
+  "July 29, 2008 10:00:00 PST"
+);
+
+console.log(`I've enjoid "${whatITalk.name}", by ${whatITalk.author}`);
+console.log("This book is", whatITalk.bookAge(), "years old.");
+
+window.eloquent = eloquent;
+window.whatITalk = whatITalk;
