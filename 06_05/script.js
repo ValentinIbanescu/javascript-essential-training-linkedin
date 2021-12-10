8158609d2594fd6aa164e15c8825c491c494d262
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-12-10 09:11:27 +0200

    const

--- 06_05/script.js
+++ 06_05/script.js
@@ -3,12 +3,13 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
  */
 
-let color = "purple";
+const color = "purple";
 
 document.querySelector(".left").style.backgroundColor = color;
 document.querySelector(".left .color-value").innerHTML = color;
 
-color = "skyblue";
+// We cannot reassign the const variables
+// color = "skyblue"; NOT WORKING
 
 function headingColor() {
   let color = "blue";
