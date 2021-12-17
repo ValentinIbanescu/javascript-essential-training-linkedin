5463a949ba074f645da24aaabd62046971d4c219
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-12-17 18:11:16 +0200

    variables

--- 06_02/script.js
+++ 06_02/script.js
@@ -2,3 +2,24 @@
  * The var statement
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
  */
+var test = 5;
+console.log(test);
+
+test = "OK";
+console.log(test);
+
+something = 7;
+console.log(something);
+
+console.log(typeof something);
+
+var x = 4,
+  y = "red",
+  z = { name: "Anna", age: 45 };
+
+console.log(x, y, z);
+
+console.log(typeof z);
+
+var empty;
+console.log(empty);
