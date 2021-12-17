62a427f1f6592ccbd10673c3788d5b80c30b9547
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-12-17 16:11:16 +0200

    practice class attributes manipulation

--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,31 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+
+// List classes
+document.querySelector("#pack01 img").classList;
+// Add two new classes
+document.querySelector("#pack01 img").classList.add("rotated");
+document.querySelector("#pack01 img").classList.add("border");
+// Remove one class
+document.querySelector("#pack01 img").classList.remove("border");
+// List attributes
+document.querySelector("#pack01 img").attributes;
+// Check attribute
+document.querySelector("#pack01 img").hasAttribute("alt");
+// Set attribute
+document
+  .querySelector("#pack01 img")
+  .setAttribute("alt", "backpack image grey");
+// Get data attribute
+document
+  .querySelector("#pack01 ul li.backpack__strap")
+  .getAttribute("data-side");
+// Change attribute
+document
+  .querySelector("#pack01 ul li.backpack__strap")
+  .setAttribute("data-side", "left-up");
+// Set style
+document.querySelector("#pack01 h1").style.color = "red";
+// List style properties
+document.querySelector("#pack01 h1").style;
