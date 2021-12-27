c63892d465b93edeb101e4c70b12d6a4bfc4b2bc
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2021-12-27 13:27:27 +0200

    event listeners

--- Practice/09_04/script.js
+++ Practice/09_04/script.js
@@ -5,3 +5,49 @@
  * - Add an event listener to each grid cell to change its background color when it is clicked.
  * - Add an event listener to a specific key on the keyboard to change the background color of the whole page - from dark to light and back again.
  */
+
+// * - Use an event listener and CSS either inline or through an added class
+// to draw a highlight around the entire grid when you hover over it with your mouse.
+let entireGrid = document.querySelector(".grid");
+console.log(entireGrid);
+entireGrid.addEventListener("mouseenter", (event) => {
+  console.log(event);
+  entireGrid.classList.add("highlight");
+});
+
+entireGrid.addEventListener("mouseleave", (event) => {
+  console.log(event);
+  entireGrid.classList.remove("highlight");
+});
+
+let randColor = () => {
+  let hexColor = Math.floor(Math.random() * 16777215).toString(16);
+  return `#${hexColor}`;
+};
+console.log(randColor());
+
+let cellsGrid = document.querySelectorAll(".cell");
+console.log(cellsGrid);
+
+for (const oneCell of cellsGrid) {
+  oneCell.addEventListener("mouseenter", (event) => {
+    console.log(event);
+    oneCell.style.backgroundColor = randColor();
+  });
+}
+
+for (const oneCell of cellsGrid) {
+  oneCell.addEventListener("click", (event) => {
+    console.log(event);
+    oneCell.style.backgroundColor = "white";
+  });
+}
+const fullPage = document.querySelector("body");
+fullPage.addEventListener("keydown", (event) => {
+  console.log(event);
+  if (event.key === "b" || event.key === "B") {
+    fullPage.style.backgroundColor = "black";
+  } else {
+    fullPage.style.backgroundColor = "white";
+  }
+});
