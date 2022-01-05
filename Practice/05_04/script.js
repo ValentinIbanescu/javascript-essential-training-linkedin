852025597e6a17412068d3256289448ffa7f0272
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2022-01-05 15:11:11 +0200

    querySelector advanced selectors

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,4 +2,29 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
 document.querySelectorAll("h1").forEach((item) => (item.style.color = "red"));
+
+// Class after pseudo-element
+console.log(
+  getComputedStyle(
+    document.querySelector(".siteheader"),
+    ":after"
+  ).getPropertyValue("content")
+);
+
+// First of type element
+document.querySelector("button:first-of-type").style.boxShadow =
+  "0.3em 0.3em 1em rgba(25, 225, 255, 0.7)";
+
+// Nested last child
+document.querySelector(".rightLength button:last-child").style.boxShadow =
+  "0.3em 0.3em 1em rgba(225, 25, 25, 0.7)";
+
+// Nested nth-child
+document.querySelector("#pack02 li:nth-child(5) span").style.color = "blue";
+
+// Nested select more items
+document
+  .querySelectorAll("#pack01 li:nth-child(odd)")
+  .forEach((item) => (item.style.color = "red"));
