08ba026da3650fe664be8e3d0fdc7793d1584fc9
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2022-01-13 21:24:43 +0200

    return data

--- 08_08/script.js
+++ 08_08/script.js
@@ -8,15 +8,23 @@
  * @link http://www.lingoes.net/en/translator/langcode.htm
  */
 
-const tipCalculator = (sum, percentage) => {
+const formatter = (value, locale, currency) => {
+  const formattedValue = new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency: currency,
+  }).format(value);
+  return formattedValue;
+};
+
+const tipCalculator = (sum, percentage, locale, currency) => {
   let tip = sum * (percentage / 100);
   let total = sum + tip;
   console.log(`
-      Sum before tip: ${sum}
+      Sum before tip: ${formatter(sum, locale, currency)}
       Tip percentage: ${percentage}%
-      Tip:            ${tip.toFixed(2)}
-      Total:          ${total.toFixed(2)}
+      Tip:            ${formatter(tip, locale, currency)}
+      Total:          ${formatter(total, locale, currency)}
     `);
 };
 
-tipCalculator(29.95, 18);
+tipCalculator(29.95, 18, "en-US", "USD");
